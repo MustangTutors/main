528afc0dc627fca9314d9a897e8f8bafae172397
JessicaYeh <jyeh@smu.edu>
2014-03-12 09:29:09 -0500

    generate stars function

--- js/search.js
+++ js/search.js
@@ -1,6 +1,14 @@
 $(document).ready(function() {
-    // $(".tutorBox").eq(0).children(".tutorBox-extended").show();
-    
+    // Temporary stuff.
+    $(".tutorBox .ratingStars").eq(0).html(convertToStars(4.3));
+    $(".tutorBox .ratingStars").eq(1).html(convertToStars(5));
+    $(".tutorBox .ratingStars").eq(2).html(convertToStars(2.7));
+    $(".tutorBox .ratingStars").eq(3).html(convertToStars(1));
+    $(".tutorBox .ratingStars").eq(4).html(convertToStars(2.5));
+
+
+
+    // When you hover in and out of a tutor's box, it shows/hides more info.
     $(".tutorBox-small").on("mouseenter", function() {
         $(this).siblings(".tutorBox-extended").stop().animate({width:'toggle'}, 200);
     });
@@ -8,3 +16,21 @@ $(document).ready(function() {
         $(this).siblings(".tutorBox-extended").stop().animate({width:'toggle'}, 200);
     });
 });
+
+// Generate stars for the rating, given a decimal number.
+function convertToStars(num) {
+    var output = "";
+
+    // Round the number to the nearest 0.5
+    num = (Math.round(num * 2) / 2).toFixed(1);
+
+    // Add to the output the right number of stars
+    for (num; num > 0.5; num--) {
+        output += '<img src="img/star.png" alt="Rating Star">';
+    }
+    if (num !== 0) {
+        output += '<img src="img/halfstar.png" alt="Rating Star">';
+    }
+
+    return output;
+}
