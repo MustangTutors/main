960b82d6b83d9d262a6dd25dc07910de8e25141e
SZanetti <storyzanetti@gmail.com>
2014-03-12 14:17:43 -0500

    Share functionality with buttons

--- js/history.js
+++ js/history.js
@@ -3,5 +3,29 @@ $(document).ready(function(){
 	$(document).on('mouseover', '#share', function(){
 		// Make "Share" message appear
 		$('#share_message').css('display', 'inline-block');
+
+		// Change share icon to gray
+		$('#share').attr('src', 'img/share_gray.png');
+	});
+
+	// Remove "share" message and change share icon back to black
+	$(document).on('mouseout', '#share', function(){
+		// Make "Share" message disappear
+		$('#share_message').hide();
+
+		// Change share icon to black
+		$('#share').attr('src', 'img/share.png');
+	});
+
+	// Show share window when the share icon is clicked
+	$(document).on('click', '#share', function(){
+		// Show share window
+		$('#share_window').show();
+	});
+
+	// Hide share window when cancel is clicked
+	$(document).on('click', '#share_cancel', function(){
+		// Hide share window
+		$('#share_window').hide();
 	});
 });
