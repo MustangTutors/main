c7d350216e00a11fdef532e7df320849ad47f7cc
JessicaYeh <jyeh@smu.edu>
2014-03-12 01:35:04 -0500

    this page is beautiful. and I should go to sleep now.

--- js/search.js
+++ js/search.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    //$(".tutorBox").eq(0).children(".tutorBox-extended").show();
+    // $(".tutorBox").eq(0).children(".tutorBox-extended").show();
     
     $(".tutorBox-small").on("mouseenter", function() {
         $(this).siblings(".tutorBox-extended").stop().animate({width:'toggle'}, 200);
