29a57ffbc0bdb440ade5120373f8fbb0d2aaca52
JessicaYeh <jyeh@smu.edu>
2014-03-12 00:50:28 -0500

    added some sexy effects

--- js/search.js
+++ js/search.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
     $(".tutorBox").on("mouseenter", function() {
-        $(this).children(".tutorBox-extended").css("display", "block");
+        $(this).children(".tutorBox-extended").stop().slideDown();
     });
     $(".tutorBox").on("mouseleave", function() {
-        $(this).children(".tutorBox-extended").css("display", "none");
+        $(this).children(".tutorBox-extended").stop().slideUp();
     });
 });
