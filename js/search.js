3f64d705aabfdb14ebf5cb6f01de87939e288d9f
JessicaYeh <jyeh@smu.edu>
2014-03-12 01:19:33 -0500

    styling available/busy

--- js/search.js
+++ js/search.js
@@ -1,9 +1,10 @@
 $(document).ready(function() {
+    //$(".tutorBox").eq(0).children(".tutorBox-extended").show();
+    
     $(".tutorBox-small").on("mouseenter", function() {
-        $(this).siblings(".tutorBox-extended").stop().animate({width:'toggle'}, 250);
+        $(this).siblings(".tutorBox-extended").stop().animate({width:'toggle'}, 200);
     });
     $(".tutorBox-small").on("mouseleave", function() {
-        $(this).siblings(".tutorBox-extended").stop().animate({width:'toggle'}, 250);
-
+        $(this).siblings(".tutorBox-extended").stop().animate({width:'toggle'}, 200);
     });
 });
