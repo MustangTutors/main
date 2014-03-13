76726d0e927db5b9284e4476b0a3002dea71d611
JessicaYeh <jyeh@smu.edu>
2014-03-13 12:35:54 -0500

    added approve/deny images

--- js/applications.js
+++ js/applications.js
@@ -1,5 +1,16 @@
 $(document).ready(function() {
     $(document).on('click', '.approve', function() {
-        $(this).parent().html('<button class="edit button">Edit Profile</button>');
+        var button = $(this);
+        var status = button.parents('.applicationReview').find('img.applicationStatus');
+        status.attr('src', 'img/approved.png');
+        status.css('visibility', 'visible');
+        button.parent().html('<button class="edit button">Edit Profile</button>');
+    });
+    $(document).on('click', '.deny', function() {
+        var button = $(this);
+        var status = button.parents('.applicationReview').find('img.applicationStatus');
+        status.attr('src', 'img/denied.png');
+        status.css('visibility', 'visible');
+        button.parent().html('');
     });
 });
