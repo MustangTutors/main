0df36d2fd636000a4b6584947e85f953cd1f99d3
SZanetti <storyzanetti@gmail.com>
2014-03-12 19:09:20 -0500

    Toggle color change is almost working

--- js/include.js
+++ js/include.js
@@ -2,27 +2,46 @@ $(document).ready(function() {
     $("header").load("header.html");
     $("footer").load("footer.html");
 
-    changeToggleColor();
+    setToggleColor();
 
     // Change font color on toggle switch
-    $(document).on('change', 'label.toggle input[type="checkbox"]', changeToggleColor);
+    $(document).on('change', 'label.toggle input[type="checkbox"]', function(){
+        var which = $(this).prop('checked');
+
+        if(which){
+            $(this).siblings('span').children('span.false').css('color', '#AAA');
+            $(this).siblings('span').children('span.true').css('color', 'white');
+        }
+        else {
+            $(this).siblings('span').children('span.false').css('color', 'white');
+            $(this).siblings('span').children('span.true').css('color', '#AAA');
+        }
+    });
 });
 
-function changeToggleColor() {
-	if($(this)[0] !== undefined){
-		var which = $(this).prop('checked');
+// Set the color of the toggles when the page loads
+function setToggleColor() {
+    var hist_view = $('label#historyView input[type="checkbox"]').prop('checked');
 
-		if(which){
-			$(this).siblings('span').children('span.false').css('color', '#AAA');
-			$(this).siblings('span').children('span.true').css('color', 'white');
-		}
-		else {
-			$(this).siblings('span').children('span.false').css('color', 'white');
-			$(this).siblings('span').children('span.true').css('color', '#AAA');
-		}
-	}
-	else {
-		$('label.toggle input[type="checkbox"]').siblings('span').children('span.false').css('color', '#AAA');
-		$('label.toggle input[type="checkbox"]').siblings('span').children('span.true').css('color', 'white');
-	}	
+    if(hist_view){
+        $('label#history_view span.true').css('color','white');
+        $('label#history_view span.false').css('color','#AAA');
+    }
+    else{
+        $('label#history_view span.true').css('color','#AAA');
+        $('label#history_view span.false').css('color','white');
+    }
+
+    // This part doesn't work. I don't know why.
+    var tutor_in = $('#toggleButton label input[type="checkbox"]').prop('checked');
+
+    if(tutor_in){
+        
+        $('#toggleButton label span.true').css('color','white');
+        $('#toggleButton label span.false').css('color','#AAA');
+    }
+    else{
+        $('#toggleButton label span.true').css('color','#AAA');
+        $('#toggleButton label span.false').css('color','white');
+    }
 }
