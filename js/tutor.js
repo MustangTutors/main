44f5b80c275cdea6ab3bfde87036874043d3b7e1
BreshardB <breshard.busby@gmail.com>
2014-03-23 00:39:08 -0500

    Can remove rating window when clicking a rating

--- js/tutor.js
+++ js/tutor.js
@@ -1,4 +1,14 @@
 $(document).ready(function() {
+
+	var star = '<img src="img/star.png" alt="Rating Star">';
+	var empty_star = '<img src="img/emptystar.png" alt="Rating Star">';
+
+	for(var i = 0; i < 5; i++) {
+		$("span#averageRating h3 span").append(star);
+		$("span#yourRating h3 span").append(star);
+	}
+
+
 	$("input[name='addComment']").on('click', function() {
 		var date = new Date();
 
@@ -18,10 +28,16 @@ $(document).ready(function() {
 		$("textarea[name='commentBox']").val("");
 	});
 
-	var star = '<img src="img/star.png" alt="Rating Star">';
+	$("img[src='img/pencil.png']").on('click', function() {
+		$("#editRating").show();
 
-	for(var i = 0; i < 5; i++) {
-		$("span#averageRating h3 span").append(star);
-		$("span#yourRating h3 span").append(star);
-	}
+		for(var f = 0; f < 5; f++) {
+			$(".potential_rating span").append(empty_star);
+		}
+	});
+
+	$(".potential_rating").on('click', function() {
+		$(".potential_rating span img").remove();
+		$("#editRating").hide();
+	});
 });
