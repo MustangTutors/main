572fd1d70592fa0d6a3aa09faa0ef246bd1fb7b0
BreshardB <breshard.busby@gmail.com>
2014-03-27 17:53:15 -0500

    Working on ajax for login

--- js/login.js
+++ js/login.js
@@ -2,12 +2,28 @@ $(document).ready(function() {
 	$(document).on('submit', 'form#loginForm', function(e) {
 		e.preventDefault();
 
-		var id = $("input#login_id").val();
-		var password = $("input#login_password").val();
-
 		$("form#loginForm").hide();
 		$("div#welcomeMessage").show();
 
-		$("div#welcomeMessage span.welcome").append(id);
+		$.ajax({
+                type: "POST",
+                url: "Laravel/public/users/login",
+                data: {
+                	smu_id: $("input#login_id").val();
+                	password: $("input#login_password").val();
+                },
+                success: function(output) {
+                    if (output === "The user was not logged in properly") {
+                        $(".error").html("Error: The user id and password do not match our records.<br/><br/>");
+                        $("input#login_id").val("");
+                		$("input#login_password").val("");
+                    }
+                    else {
+                    	$("form#loginForm").hide();
+                    	$("div#welcomeMessage").show();
+                    	$("div#welcomeMessage span.welcome").append(output);
+                    }
+                }
+            });
 	});
 });
