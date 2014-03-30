ef6d2fad3c864fde5d90f8497c294e5c93beb0a1
Jessica Yeh <jyeh@smu.edu>
2014-03-30 17:35:38 -0500

    fixing email appearance again

--- Laravel/app/views/emails/codeword/codeword.blade.php
+++ Laravel/app/views/emails/codeword/codeword.blade.php
@@ -6,16 +6,14 @@
 <body>
     <div>
         <p> 
-        Hello,\n\nYou have been given access to a student's records through Mustang Tutors.\n\n 
+        Hello, You have been given access to a student's records through Mustang Tutors. 
         In order to access this student's records please go to 
         <a style = "color:#83192C" href = "http://mustangtutors.floccul.us/index.html">mustangtutors.floccul.us</a> 
         and click on the <span style = "color:#83192C">"Get Student's History"</span> tab.  Or you can follow this link directly to the page 
-        <a style = "color:#83192C" href="http://mustangtutors.floccul.us/findstudent.html"> Get Student's Records</a>
-        \n\n
+        <a style = "color:#83192C" href="http://mustangtutors.floccul.us/findstudent.html"> Get Student's Records</a> 
         Once on the page use this information to access their history.
-        \n\n
         <p style = "color:#3092B0">Smu Id: {{$smu_id}}\nCodeword: {{$codeword}}</p>
         </p>
-        <h3>Thanks,\nMustang Tutor's Staff</h3>
+        <h3>Thanks, Mustang Tutor's Staff</h3>
     </div>
 </body>
