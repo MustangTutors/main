a74fead25fe9a5a979294d138e4834ca61daa7d0
SZanetti <storyzanetti@gmail.com>
2014-04-21 15:36:52 -0500

    Redirect page from apply if not logged in

--- js/apply.js
+++ js/apply.js
@@ -1,3 +1,14 @@
+// Check if the user is logged in. If not, redirect to findstudent.html
+$.ajax({
+    url: "Laravel/public/users/current",
+    success: function(json) {
+        json = JSON.parse(json);
+        if (json.length === 0) {
+            window.location.href = "index.html";
+        }
+    }
+});
+
 $(document).ready(function() {
 
 	var user_id = getURLParameter('user_id');
