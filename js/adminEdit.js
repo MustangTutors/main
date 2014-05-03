7510c4061a7abde62f1242ee6531cbd891514567
SZanetti <storyzanetti@gmail.com>
2014-05-02 20:03:18 -0500

    Fixed adminEdit so that the pencils no longer appear

--- js/adminEdit.js
+++ js/adminEdit.js
@@ -1,10 +1,9 @@
+var list_of_course_ids = [];
+
+// Get the user_id from query in URL, then get tutor info
+var user_id = getURLParameter('user_id');
+
 $(document).ready(function() {
-
-	// Get the user_id from query in URL, then get tutor info
-	var user_id = getURLParameter('user_id');
-
-    $("section.tutor_info").removeClass("admin_tutor_info");
-    $(".edit_button").hide();
 
     // Hides the Your Rating section if the user is the same person as the tutor.
     // Also hides the comment form.
@@ -34,11 +33,6 @@ $(document).ready(function() {
 
 	$("article.schedule").css("height", "295px");
 
-    var list_of_course_ids = [];
-    var list_of_days = [null, null, null, null, null, null, null];
-    var list_of_start_times = [null, null, null, null, null, null, null];
-    var list_of_end_times = [null, null, null, null, null, null, null];
-
     var tutorInfo;
 	$.ajax({
         type: "GET",
@@ -47,12 +41,15 @@ $(document).ready(function() {
 
             tutorInfo = JSON.parse(output);
 
+            // Set Tutor's name
             var name = tutorInfo.tutor_fName + " " + tutorInfo.tutor_lName;
             $("section#rating h2").html(name);
 
+            // Set Tutor's profile picture
             var profile_pic = "img/tutors/" + tutorInfo.tutor_id + ".jpg";
             $(".tutorPicture img").attr('src', profile_pic);
  
+            // Set Tutor's rating
             $("span#averageRating h3 span").html(convertToStars(tutorInfo.average_rating));
 
             var num_ratings = Number(tutorInfo.numberOfRatings);
@@ -64,6 +61,7 @@ $(document).ready(function() {
             }
 			$("span#yourRating h3 span").html(convertToStars(tutorInfo.current_user_rating));
 
+            // Set if Tutor is Active or Inactive
             if (Number(tutorInfo.active) === 0) {
                 $("span#tutorpage_available").html("Disabled");
                 $("span#tutorpage_available").addClass("disabled");
@@ -74,80 +72,34 @@ $(document).ready(function() {
             else {
                 $("span#tutorpage_available").html(available[tutorInfo.available]);
                 $("span#tutorpage_available").addClass(available[tutorInfo.available].toLowerCase());
-            }
-
-			for(var i = 0; i < tutorInfo.courses.length; i++) {
-				var index = i+1;
-				var course = "<span class='label'>";
-                course += "<input type='checkbox'>";
-				course += tutorInfo.courses[i].subject+" "+tutorInfo.courses[i].course_number+":";
-				course += "</span><span class='content'>";
-				course += tutorInfo.courses[i].course_name;
-				course += "</span>";
-				$("article#courses ul").append("<li>");
-				$("article#courses ul li:nth-child("+index+")").append(course);
-				$("article#courses ul").append("</li>");
-                list_of_course_ids[i] = tutorInfo.courses[i].course_id;
-			}
-
-            if (!tutorInfo.hours) {
-                tutorInfo.hours = [];
-            }
-            for(var dayIndex = 0, tutorDayIndex = 0; dayIndex < days.length; dayIndex++) {
-                var day = $('<li><span class="day"></span><span class="content"></span></li>');
-                day.find('.day').html(days[dayIndex]);
-                if ((tutorDayIndex < tutorInfo.hours.length) && (Number(tutorInfo.hours[tutorDayIndex].day)-1 === dayIndex)) {
-                    day.find('.content').html(convertTime(tutorInfo.hours[tutorDayIndex].start_time)+" to "+convertTime(tutorInfo.hours[tutorDayIndex].end_time));
-                    
-                    list_of_days[dayIndex] = Number(tutorInfo.hours[tutorDayIndex].day);
-                    
-                    list_of_start_times[dayIndex] = tutorInfo.hours[tutorDayIndex].start_time;
-
-                    list_of_end_times[dayIndex] = tutorInfo.hours[tutorDayIndex].end_time;
-
-                    tutorDayIndex++;
-                }
-                else {
-                    day.find('.content').html("N/A");
-                }
-                $("article#hours ul").append(day);
-            }
-
-            $("input[type='checkbox']").hide();
+            }            
         }
     });
 
-    $.ajax({
-        type: "GET",
-        url: "Laravel/public/courses/showAll",
-        success: function(courses) {
-            courses = JSON.parse(courses);
-            for(var i = 0; i < courses.length; i++) {
-                var option = "<option>";
-                option += courses[i].course_id + " " + courses[i].subject + " " + courses[i].course_number + " " + courses[i].course_name;
-                option += "</option>";
-                $("select.course_dropdown").append(option);
-            }
-        }
-    });
+    populateCourses();
+    populateHours();
 
     var potential = 0;
 
+    // Add new course dropdown when add button clicked
     $("img[src='img/add.png']").on("click", function(e) {
-
         e.preventDefault();
+
+        console.log("here");
 
         $("article#courses ul").append(
             "<li class='potential_course new_dropdown'>" +
                 "<select id='potential" + potential + "' class='course_dropdown'>" +
-                    "</select></li>"
-        );
+                    "</select></li>");
 
         $.ajax({
             type: "GET",
             url: "Laravel/public/courses/showAll",
             success: function(courses) {
                 courses = JSON.parse(courses);
+
+                $("select.course_dropdown").append('<option value="" disabled selected>Choose a course</option>');
+
                 for(var i = 0; i < courses.length; i++) {
                     var option = "<option>";
                     option += courses[i].course_id + " " + courses[i].subject + " " + courses[i].course_number + " " + courses[i].course_name;
@@ -168,133 +120,23 @@ $(document).ready(function() {
             var new_height = $("section.tutor_info article").height();
             $("section.tutor_info article").height(new_height+30);
         }
-        
     });
 
-    $("article#courses img[src='img/pencil.png']").on("click", function(e) {
-        e.preventDefault();
-        $("section.tutor_info").addClass("admin_tutor_info");
-        $("article#courses .edit_button").show();
-        $("input[type='checkbox']").show();
-        $("span.label input[type='checkbox']").prop("checked", true);
-    });
-
-    $("article#hours img[src='img/pencil.png']").on("click", function(e) {
-        e.preventDefault();
-        $("section.tutor_info").addClass("admin_tutor_info");
-        $("article#hours .edit_button").show();
-        $("article#hours ul").html(
-
-                        '<li>' +
-                            '<span class="potential_day">' +
-                                '<input type="checkbox" id="sunday">' +
-                                    '<label for="sunday">Sunday</label>' +
-                            '</span>' +
-                            '<span class="start_end">' +
-                                '<input type="time" class="start_time" value="00:00">' +
-                                '<span> to </span>' +
-                                '<input type="time" class="end_time" value="00:00">' +
-                            '</span>' +
-                        '</li>' +
-                                
-                        '<li>' +
-                            '<span class="potential_day">' +
-                                '<input type="checkbox" id="monday">' +
-                                '<label for="monday">Monday</label>' +
-                            '</span>' +
-                            '<span class="start_end">' +
-                                '<input type="time" class="start_time" value="00:00">' +
-                                '<span> to </span>' +
-                                '<input type="time" class="end_time" value="00:00">' +
-                            '</span>' +
-                        '</li>' +
-                
-                        '<li>' +
-                            '<span class="potential_day">' +
-                                '<input type="checkbox" id="tuesday">' +
-                                '<label for="tuesday">Tuesday</label>' +
-                            '</span>' +
-                            '<span class="start_end">' +
-                                '<input type="time" class="start_time" value="00:00">' +
-                                '<span> to </span>' +
-                                '<input type="time" class="end_time" value="00:00">' +
-                            '</span>' +
-                        '</li>' +
-                
-                        '<li>' +
-                            '<span class="potential_day">' +
-                                '<input type="checkbox" id="wednesday">' +
-                                '<label for="wednesday">Wednesday</label>' +
-                            '</span>' +
-                            '<span class="start_end">' +
-                                '<input type="time" class="start_time" value="00:00">' +
-                                '<span> to </span>' +
-                                '<input type="time" class="end_time" value="00:00">' +
-                            '</span>' +
-                        '</li>' +
-                
-                        '<li>' +
-                            '<span class="potential_day">' +
-                                '<input type="checkbox" id="thursday">' +
-                                '<label for="thursday">Thursday</label>' +
-                            '</span>' +
-                            '<span class="start_end">' +
-                                '<input type="time" class="start_time" value="00:00">' +
-                                '<span> to </span>' +
-                                '<input type="time" class="end_time" value="00:00">' +
-                            '</span>' +
-                        '</li>' +
-                
-                        '<li>' +
-                            '<span class="potential_day">' +
-                                '<input type="checkbox" id="friday">' +
-                                '<label for="friday">Friday</label>' +
-                            '</span>' +
-                            '<span class="start_end">' +
-                                '<input type="time" class="start_time" value="00:00">' +
-                                '<span> to </span>' +
-                                '<input type="time" class="end_time" value="00:00">' +
-                            '</span>' +
-                        '</li>' +
-                
-                        '<li>' +
-                            '<span class="potential_day">' +
-                                '<input type="checkbox" id="saturday">' +
-                                '<label for="saturday">Saturday</label>' +
-                            '</span>' +
-                            '<span class="start_end">' +
-                                '<input type="time" class="start_time" value="00:00">' +
-                                '<span> to </span>' +
-                                '<input type="time" class="end_time" value="00:00">' +
-                            '</span>' +
-                        '</li>');
-
-        var days = $("article#hours ul li input[type='checkbox']");
-        var start_times = $("article#hours ul li input.start_time");
-        var end_times = $("article#hours ul li input.end_time");
-
-        for(var i = 0; i < list_of_days.length; i++) {
-            if(list_of_days[i] !== null) {
-                days.eq(i).prop("checked", true);
-                start_times.eq(i).val(list_of_start_times[i].substring(0, 5));
-                end_times.eq(i).val(list_of_end_times[i].substring(0, 5));
-            }
-        }
-    });
-
+    // Cancel changes to courses
     $("button[name='cancelCourseChanges']").on("click", function(e) {
         e.preventDefault();
-
         $("li.new_dropdown").remove();
-        $("section.tutor_info article").height(295);
+        $("article#courses input[type='checkbox']").prop('checked', true);
     });
 
+    // Cancel changes to hours
     $("button[name='cancelHourChanges']").on("click", function(e) {
         e.preventDefault();
-
-        $("input[type='time']").val("00:00");
+        populateHours();
     });
 
+
+    // Update courses
     $("button[name='saveCourseChanges']").on("click", function(e) {
         e.preventDefault();
 
@@ -337,8 +179,10 @@ $(document).ready(function() {
         $("span#course_confirmation").show();
         $("button[name='saveCourseChanges']").css("margin-top", -20);
         $("button[name='cancelCourseChanges']").css("margin-top", -20);
+        $("article#courses img[src='img/pencil.png']").show();
     });
 
+    // Update hours
     $("button[name='saveHourChanges']").on("click", function(e) {
         e.preventDefault();
 
@@ -373,8 +217,10 @@ $(document).ready(function() {
         $("span#hour_confirmation").show();
         $("button[name='saveHourChanges']").css("margin-top", -20);
         $("button[name='cancelHourChanges']").css("margin-top", -20);
+        $("article#hours img[src='img/pencil.png']").show();
     });
 
+    // Toggle whether tutor is currently employed or not
     $(document).on('change', '#active_toggle_switch input[type="checkbox"]', function() {
         $.ajax({
             type: "GET",
@@ -406,3 +252,138 @@ $(document).ready(function() {
     })
 
 });
+
+function populateHours() {
+    $("article#hours ul").html(
+        '<li>' +
+            '<span class="potential_day">' +
+                '<input type="checkbox" id="sunday">' +
+                    '<label for="sunday">Sunday</label>' +
+            '</span>' +
+            '<span class="start_end">' +
+                '<input type="time" class="start_time" value="00:00">' +
+                '<span> to </span>' +
+                '<input type="time" class="end_time" value="00:00">' +
+            '</span>' +
+        '</li>' +
+                
+        '<li>' +
+            '<span class="potential_day">' +
+                '<input type="checkbox" id="monday">' +
+                '<label for="monday">Monday</label>' +
+            '</span>' +
+            '<span class="start_end">' +
+                '<input type="time" class="start_time" value="00:00">' +
+                '<span> to </span>' +
+                '<input type="time" class="end_time" value="00:00">' +
+            '</span>' +
+        '</li>' +
+
+        '<li>' +
+            '<span class="potential_day">' +
+                '<input type="checkbox" id="tuesday">' +
+                '<label for="tuesday">Tuesday</label>' +
+            '</span>' +
+            '<span class="start_end">' +
+                '<input type="time" class="start_time" value="00:00">' +
+                '<span> to </span>' +
+                '<input type="time" class="end_time" value="00:00">' +
+            '</span>' +
+        '</li>' +
+
+        '<li>' +
+            '<span class="potential_day">' +
+                '<input type="checkbox" id="wednesday">' +
+                '<label for="wednesday">Wednesday</label>' +
+            '</span>' +
+            '<span class="start_end">' +
+                '<input type="time" class="start_time" value="00:00">' +
+                '<span> to </span>' +
+                '<input type="time" class="end_time" value="00:00">' +
+            '</span>' +
+        '</li>' +
+
+        '<li>' +
+            '<span class="potential_day">' +
+                '<input type="checkbox" id="thursday">' +
+                '<label for="thursday">Thursday</label>' +
+            '</span>' +
+            '<span class="start_end">' +
+                '<input type="time" class="start_time" value="00:00">' +
+                '<span> to </span>' +
+                '<input type="time" class="end_time" value="00:00">' +
+            '</span>' +
+        '</li>' +
+
+        '<li>' +
+            '<span class="potential_day">' +
+                '<input type="checkbox" id="friday">' +
+                '<label for="friday">Friday</label>' +
+            '</span>' +
+            '<span class="start_end">' +
+                '<input type="time" class="start_time" value="00:00">' +
+                '<span> to </span>' +
+                '<input type="time" class="end_time" value="00:00">' +
+            '</span>' +
+        '</li>' +
+
+        '<li>' +
+            '<span class="potential_day">' +
+                '<input type="checkbox" id="saturday">' +
+                '<label for="saturday">Saturday</label>' +
+            '</span>' +
+            '<span class="start_end">' +
+                '<input type="time" class="start_time" value="00:00">' +
+                '<span> to </span>' +
+                '<input type="time" class="end_time" value="00:00">' +
+            '</span>' +
+        '</li>');
+
+    $.ajax({
+        type: "GET",
+        url: "Laravel/public/tutor/" + user_id,
+        success: function(json) {
+            json = JSON.parse(json);
+            json = json.hours;
+
+            var days = $('article#hours .potential_day input[type="checkbox"]');
+            var start_times = $('article#hours input.start_time[type="time"]');
+            var end_times = $('article#hours input.end_time[type="time"]');
+
+            for(var i = 0; i < json.length; i++){
+                var index = json[i].day;
+                console.log(index);
+                days.eq(index).prop('checked', true);
+                start_times.eq(index).val(json[i].start_time);
+                end_times.eq(index).val(json[i].end_time);
+            }
+        }
+    });
+}
+
+function populateCourses(){
+    $.ajax({
+        type: "GET",
+        url: "Laravel/public/tutor/" + user_id,
+        success: function(json){
+            json = JSON.parse(json);
+            json = json.courses;
+            // Loop through courses and add
+            for(var i = 0; i < json.length; i++) {
+                var index = i+1;
+                var course = "<span class='label'>";
+                course += "<input type='checkbox'>";
+                course += json[i].subject + " " + json[i].course_number + ":";
+                course += "</span><span class='content'>";
+                course += json[i].course_name;
+                course += "</span>";
+                $("article#courses ul").append("<li>");
+                $("article#courses ul li:nth-child("+index+")").append(course);
+                $("article#courses ul").append("</li>");
+                list_of_course_ids[i] = json[i].course_id;
+            }
+
+            $("article#courses input[type='checkbox']").prop('checked', true);
+        }
+    });
+}
